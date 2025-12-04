16c903352cb9831d205cdbf3ec8025ee003f562e
Greenbydefault <software@greenbydefault.de>
2025-12-04 13:31:54 +0100

    fix(strategie): Kategorien-Button nach rechts
    - Hinzufügen links, Kategorien rechts (space-between)
    - Konsistent mit anderen Seiten

--- src/modules/strategie/StrategieDetail.js
+++ src/modules/strategie/StrategieDetail.js
@@ -80,13 +80,15 @@ export class StrategieDetail {
   renderAddItemSection() {
     return `
       <div class="add-item-section add-item-section--compact">
-        <div class="add-item-actions">
+        <div class="add-item-actions-left">
           <button type="button" class="primary-btn" id="btn-open-add-drawer">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" style="width: 16px; height: 16px;">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
             Hinzufügen
           </button>
+        </div>
+        <div class="add-item-actions-right">
           <button type="button" class="secondary-btn" id="btn-manage-kategorien" title="Kategorien verwalten">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" style="width: 16px; height: 16px;">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
