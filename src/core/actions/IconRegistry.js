0b3cd15e0872017037998f081870a1a9ba9840ac
Greenbydefault <software@greenbydefault.de>
2025-12-04 13:39:49 +0100

    fix(icons): Unlink-Icon für 'Idee von Video entfernen' getauscht
    - Neues Minus-Kreis Icon statt buggy Link-Icon

--- src/core/actions/IconRegistry.js
+++ src/core/actions/IconRegistry.js
@@ -44,7 +44,7 @@ export class IconRegistry {
 </svg>`,
       unlink: `
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M13.181 8.68a4.503 4.503 0 0 1 1.903 6.405m-9.768-2.782L3.56 14.06a4.5 4.5 0 0 0 6.364 6.365l3.129-3.129m5.614-5.615 1.757-1.757a4.5 4.5 0 0 0-6.364-6.365l-4.5 4.5" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>`,
       'add-ansprechpartner': `
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
